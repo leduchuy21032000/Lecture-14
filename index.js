668e70e6d53601bfa9b08ebd7c38208c44311bdc
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-06-05 20:09:35 +0700

    add image

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-console.log("Hello world!")
-
-console.log("Huy Test 1")
-
-console.log("Son demo mot -> commit thu 12")
-console.log("Hello Test 2")
+function clicker() {
+    console.log("you click to image")
+}
+function generateImage(image) {
+    return `<img src=${image} onclick="clicker()"/>`
+}
+let imgURL = "https://estnn.com/wp-content/uploads/2019/11/Nigma-Esports-300x300.jpg"
+document.querySelector("body").insertAdjacentHTML("beforeend", generateImage(imgURL))
