1d3240c1754d60c02ae0ff140a5121463b3da3c2
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-06-05 20:12:18 +0700

    add round class

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function clicker() {
     console.log("you click to image")
 }
 function generateImage(image) {
-    return `<img src=${image} onclick="clicker()"/>`
+    return `<img class="round" src=${image} onclick="clicker()"/>`
 }
 let imgURL = "https://estnn.com/wp-content/uploads/2019/11/Nigma-Esports-300x300.jpg"
 document.querySelector("body").insertAdjacentHTML("beforeend", generateImage(imgURL))
