19861898c8e55b884e32cb1a28cc4c6fa8aa2d57
leduchuy21032000@gmail.com <leduchuy21032000@gmail.com>
2021-06-05 20:11:20 +0700

    Xoa img

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function clicker() {
-    console.log("you click to image")
-}
-function generateImage(image) {
-    return `<img src=${image} onclick="clicker()"/>`
-}
-let imgURL = "https://estnn.com/wp-content/uploads/2019/11/Nigma-Esports-300x300.jpg"
-document.querySelector("body").insertAdjacentHTML("beforeend", generateImage(imgURL))
+// function clicker() {
+//     console.log("you click to image")
+// }
+// function generateImage(image) {
+//     return `<img src=${image} onclick="clicker()"/>`
+// }
+// let imgURL = "https://estnn.com/wp-content/uploads/2019/11/Nigma-Esports-300x300.jpg"
+// document.querySelector("body").insertAdjacentHTML("beforeend", generateImage(imgURL))
+
+console.log("Không thích hình cũ nên xóa")
